e2e76b9174e06428e827ede2dbf40be34e0acf8c
jedidiah <jedaqsaul@gmail.com>
2026-01-02 18:31:44 +0300

    create order summary function

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,19 @@
-export const cart = [];
+import { products } from "./products.js";
+
+export let cart = JSON.parse(localStorage.getItem("cart")) || [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 export function addToCart(productId) {
   const selector = document.querySelector(`.js-quantity-selector-${productId}`);
   console.log(selector);
@@ -20,4 +35,29 @@ export function addToCart(productId) {
       quantity,
     });
   }
+  saveToStorage();
 }
+
+export function findMatchingProduct(productId) {
+  let matchingProduct;
+
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+
+  return matchingProduct;
+}
+
+export const removeFromCart = (productId) => {
+  let newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
+  saveToStorage();
+};
---
+++ utils/money.js
@@ -0,0 +1,4 @@
+const formatCurrency = (priceCents) => {
+  return (Math.round(priceCents) / 100).toFixed(2);
+};
+export default formatCurrency;
